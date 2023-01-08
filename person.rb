fd8160fb26ddbee07ab4b486acf781ec1e9037ef
erickhansx <erickhansx@gmail.com>
2023-01-07 18:39:18 -0600

    Created associations between classes

--- person.rb
+++ person.rb
@@ -2,16 +2,20 @@ require './nameable'
 require './decorator'
 
 class Person < Nameable
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  attr_accessor :rentals, :id, :parent_permission, :name, :age
+
+  def initialize(age, name: 'Unknown', parent_permission: true)
     super()
     @id = Random.rand(1...1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
-  attr_reader :id
-  attr_accessor :name, :age
+  def add_rental(rental)
+    @rentals.push(rental)
+  end
 
   def can_use_services?
     @age >= 18 || @parent_permission == true
