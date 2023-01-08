30ffbf48a5b2faef98ffb3fe2aab0e65cad19bf6
erickhansx <erickhansx@gmail.com>
2023-01-07 21:13:11 -0600

    Fix add teacher error

--- teacher.rb
+++ teacher.rb
@@ -1,8 +1,9 @@
 require './person'
 
 class Teacher < Person
-  def initialize(age, specialization, name = 'Unknown', parent_permission = 'Unknown')
-    super(age, name, parent_permission)
+  attr_accessor :specialization
+  def initialize(age, specialization, name: 'Unknown')
+    super(age, name: name, parent_permission: true)
     @specialization = specialization
   end
 
